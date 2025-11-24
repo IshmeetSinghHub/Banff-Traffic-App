f52dc460fda43db59fbafa064c9c0040a0ae2c00
Ishmeet <isingh814@norquest.ca>
2025-11-23 20:48:05 -0700

    Initialize basic Streamlit app structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ st.set_page_config(page_title="Banff Traffic App - Step 1", layout="centered")
 st.title("Banff Traffic Management – Streamlit App")
 
 
+git add streamlit_app.py
+git commit -m "Step 1: basic working Streamlit app"
+git push
