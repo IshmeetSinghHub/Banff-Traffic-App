434769ab7bcd3a1464fab62fb24ebc8615696129
Ishmeet <isingh814@norquest.ca>
2025-11-23 20:51:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,4 @@ st.set_page_config(page_title="Banff Traffic App - Step 1", layout="centered")
 st.title("Banff Traffic Management – Streamlit App")
 
 
-git add streamlit_app.py
-git commit -m "Step 1: basic working Streamlit app"
-git push
-
 
